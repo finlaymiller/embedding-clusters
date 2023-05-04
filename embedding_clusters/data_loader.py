87fca3486f3206951f5e6125e0ea6093c7d523ab
Finlay Miller <finlaymiller@swelladvantage.com>
2023-05-04 10:17:46 -0700

    removed hf datasets, compatability issues with hear

--- embedding_clusters/data_loader.py
+++ embedding_clusters/data_loader.py
@@ -1,10 +1,10 @@
 import os
-from datasets import load_dataset, Audio
 
 class DataLoader():
     def __init__(self, args):
         self.input = args.input
         self.output = args.output
+        self.files = []
 
         if not os.path.exists(self.input):
             print("Problem loading data: Input folder does not exist.")
@@ -15,8 +15,13 @@ class DataLoader():
         print(f"Dataloader instantiated.\nInput folder is {self.input}\nOutput folder is {self.output}")
 
     def load(self, count=390):
-        d = load_dataset("Fhrozen/FSD50k", split="test")
-        print(d)
+        while len(self.files) < count:
+            i = 0
+            for file in os.listdir(self.input):
+                filename = os.fsdecode(file)
+                if filename.endswith(".wav"):
+                    print(f"{i} {filename}")
+                    i += 1
 
     def load_random(self, count=100):
         print(f"randomly sampling {count} files")
